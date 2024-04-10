c07f2d02f4cb149bbebc98adde3275d30ce4a992
everton <evertonhbms5@gmail.com>
2024-04-10 18:06:32 -0300

    criação da pagina home

--- mercado/views.py
+++ mercado/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import  Produto, Vendas, Compra
 
-# Create your views here.
+
+def mercado_home(request):
+    produto = Produto.objects.all()
+    return render(request,"mercado/mercado_home.html",{ "produto" : produto})
+
+def mercado_home(request):
+    vendas = Vendas.objects.all()
+    return render(request,"mercado/mercado_home.html",{ "vendas" : vendas})
