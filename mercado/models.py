fc2139a529fc3a11211ae2254611ca15aefd8fdf
everton <evertonhbms5@gmail.com>
2024-04-10 20:38:38 -0300

    ajuste front

--- mercado/models.py
+++ mercado/models.py
@@ -7,19 +7,19 @@ class Produto(models.Model):
     valor_de_venda = models.FloatField(null=False, blank= False)
     quantidade_em_estoque = models.IntegerField(null=False, blank= False)
     valor_compra = models.FloatField(null=False, blank= False)
-    data_cadastro = models.DateTimeField(auto_now_add=True)
+    data_cadastro = models.DateField(auto_now_add=True, null=False, blank= False)
 
 class Vendas(models.Model):
     codigo_venda = models.BigAutoField(auto_created=True, primary_key=True)
     produto_venda = models.ForeignKey(Produto, on_delete=models.CASCADE, null=False, blank= False)
-    data_venda = models.DateTimeField(auto_now_add=True)
+    data_venda = models.DateField(auto_now_add=True, null=False, blank= False)
     quantidade_venda = models.IntegerField(null=False, blank= False)
     fornecedor = models.CharField(max_length= 100, null=False, blank= False)
 
 
 class Compra(models.Model):
-    codigo_venda = models.BigAutoField(auto_created=True, primary_key=True)
+    codigo_compra = models.BigAutoField(auto_created=True, primary_key=True)
     produto_compra = models.ForeignKey(Produto, on_delete=models.CASCADE, null=False, blank= False)
-    data_venda = models.DateTimeField(auto_now_add=True)
-    quantidade_venda = models.IntegerField(null=False, blank= False)
+    quantidade_compra = models.IntegerField(null=False, blank= False)
     cliente = models.CharField(max_length= 100, null=False, blank= False)
+    data_compra = models.DateField(auto_now_add=True, null=False, blank= False)
--- mercado/views.py
+++ mercado/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import  Produto, Vendas, Compra
+from .models import  Produto, Compra, Vendas
 
 
 def mercado_home(request):
-    produto = Produto.objects.all()
-    return render(request,"mercado/mercado_home.html",{ "produto" : produto})
+    produtos = Produto.objects.all()
+    compras = Compra.objects.all()
+    venda = Vendas.objects.all()
+    return render(request,"mercado/mercado_home.html",
+                  { "produtos" : produtos, "compras": compras, "venda": venda })
 
-def mercado_home(request):
-    vendas = Vendas.objects.all()
-    return render(request,"mercado/mercado_home.html",{ "vendas" : vendas})
+
+
+
