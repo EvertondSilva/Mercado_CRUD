5a846df55ca0b015f1a82e8d692c3f1560b3f3f4
everton <evertonhbms5@gmail.com>
2024-04-10 21:44:28 -0300

    Criando telas e funções de Cadastro produto, venda e compra

--- mercado/views.py
+++ mercado/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import  Produto, Compra, Vendas
+from django.views.generic import CreateView  
+from django.urls import reverse_lazy
 
 
 def mercado_home(request):
@@ -9,6 +11,18 @@ def mercado_home(request):
     return render(request,"mercado/mercado_home.html",
                   { "produtos" : produtos, "compras": compras, "venda": venda })
 
+class ProdutoCreateView(CreateView):
+    model = Produto
+    fields = ["produto", "codigo_de_barrar", "valor_de_venda", "valor_compra", "quantidade_em_estoque"]
+    success_url = reverse_lazy("mercado_home")
 
+    
+class CompraCreateView(CreateView):
+    model = Compra
+    fields = ["produto_compra", "quantidade_compra","cliente"]
+    success_url = reverse_lazy("mercado_home")
 
-
+class VendasCreateView(CreateView):
+    model = Vendas
+    fields = ["produto_venda", "quantidade_venda", "fornecedor"]
+    success_url = reverse_lazy("mercado_home")
--- setup/urls.py
+++ setup/urls.py
@@ -3,9 +3,13 @@
 from django.contrib import admin
 from django.urls import path
 from mercado.views import mercado_home
+from mercado.views import ProdutoCreateView, CompraCreateView, VendasCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",mercado_home),
+    path("",mercado_home, name = "mercado_home"),
+    path("CadastroProduto", ProdutoCreateView.as_view()),
+    path("CadastroCompra", CompraCreateView.as_view()),
+    path("CadastroVendas", VendasCreateView.as_view()),
 
 ]
